684c8ae83640f3c2da42c89dcb920a2666a5a8de
PzKoT <albion_bogdan@bk.ru>
2025-11-18 22:33:04 +0300

    added ui lvl complete

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
-    [Tooltip("Имя следующей сцены для загрузки")]
     public string level = "Level2";
+    public GameObject levelCompletePanel; // UI-панель с сообщением
 
     private EnemyEntity[] skeletons;
     private int aliveCount;
+    private bool isLevelComplete = false;
 
     void Start()
     {
@@ -19,20 +22,29 @@ public class GameManager : MonoBehaviour
             if (skeleton != null)
                 skeleton.OnDeath += OnSkeletonDeath;
         }
+
+        levelCompletePanel.SetActive(false);
     }
 
     private void OnSkeletonDeath(object sender, System.EventArgs e)
     {
         aliveCount--;
-        if (aliveCount == 0)
+        if (aliveCount == 0 && !isLevelComplete)
         {
-            SceneManager.LoadScene(level);
+            isLevelComplete = true;
+            StartCoroutine(ShowLevelCompleteAndLoadNext());
         }
+    }
+
+    private IEnumerator ShowLevelCompleteAndLoadNext()
+    {
+        levelCompletePanel.SetActive(true);
+        yield return new WaitForSeconds(3); // Показываем 3 секунды
+        SceneManager.LoadScene(level);
     }
 
     private void OnDestroy()
     {
-        // Важная очистка event-подписок
         foreach (var skeleton in skeletons)
         {
             if (skeleton != null)
